dc04d26274a566eccbd77cf0a718f54c76bd95fa
EdgarCruzV <edgarcruzvillalobos@gmail.com>
2019-02-15 00:24:57 -0600

    First Commit

---
+++ app/src/main/java/com/example/challenge1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.challenge1;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttons(View view){
+        switch(view.getId()){
+            case R.id.site:
+                Uri page = Uri.parse("https://www.eatthismuch.com/");
+                Intent web = new Intent(Intent.ACTION_VIEW, page);
+                startActivity(web);
+                break;
+            case R.id.list:
+                Intent intent = new Intent(this, ListActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
